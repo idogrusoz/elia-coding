13f61a7bfb663795db1017aebb6081a9ccbb4871
Ibrahim Dogrusoz <idogrusoz@gmail.com>
2024-02-21 19:10:55 +0100

    Initial backend commit

---
+++ elia/src/main/java/com/coding/elia/EliaApplication.java
@@ -0,0 +1,13 @@
+package com.coding.elia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliaApplication.class, args);
+	}
+
+}
